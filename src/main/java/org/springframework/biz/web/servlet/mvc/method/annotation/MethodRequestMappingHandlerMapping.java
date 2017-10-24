e4c827657752500ee26653ff80760ee496223643
hnxyhcwdl1003@163.com <hnxyhcwdl1003@163.com>
2017-10-24 16:36:22 +0800

    增加springmvc请求路由逻辑

---
+++ src/main/java/org/springframework/biz/web/servlet/mvc/method/annotation/MethodRequestMappingHandlerMapping.java
@@ -0,0 +1,78 @@
+package org.springframework.biz.web.servlet.mvc.method.annotation;
+
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.core.annotation.AnnotationUtils;
+import org.springframework.util.StringUtils;
+import org.springframework.web.accept.ContentNegotiationManager;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.condition.ConsumesRequestCondition;
+import org.springframework.web.servlet.mvc.condition.HeadersRequestCondition;
+import org.springframework.web.servlet.mvc.condition.ParamsRequestCondition;
+import org.springframework.web.servlet.mvc.condition.PatternsRequestCondition;
+import org.springframework.web.servlet.mvc.condition.ProducesRequestCondition;
+import org.springframework.web.servlet.mvc.condition.RequestCondition;
+import org.springframework.web.servlet.mvc.condition.RequestMethodsRequestCondition;
+import org.springframework.web.servlet.mvc.method.RequestMappingInfo;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
+
+/**
+ * http://blog.csdn.net/home_zhang/article/details/71156093
+ * @className	： MethodRequestMappingHandlerMapping
+ * @description	： TODO(描述这个类的作用)
+ * @author 		：万大龙（743）
+ * @date		： 2017年10月24日 下午4:16:25
+ * @version 	V1.0
+ */
+@RequestMapping
+public class MethodRequestMappingHandlerMapping extends RequestMappingHandlerMapping {
+
+	private boolean useSuffixPatternMatch = true;
+
+	private boolean useTrailingSlashMatch = true;
+
+	private ContentNegotiationManager contentNegotiationManager = new ContentNegotiationManager();
+
+	private final List<String> fileExtensions = new ArrayList<String>();
+
+	private static RequestMapping requestMapping = (RequestMapping) RoutableRequestMappingHandlerMapping.class
+			.getAnnotation(RequestMapping.class);
+
+	@Override
+	protected RequestMappingInfo getMappingForMethod(Method method, Class<?> handlerType) {
+		RequestMappingInfo info = null;
+		if (requestMapping != null) {
+			RequestCondition<?> methodCondition = getCustomMethodCondition(method);
+			info = createRequestMappingInfo(requestMapping, methodCondition, method);
+			RequestMapping typeAnnotation = AnnotationUtils.findAnnotation(handlerType, RequestMapping.class);
+			if (typeAnnotation != null) {
+				RequestCondition<?> typeCondition = getCustomTypeCondition(handlerType);
+				info = createRequestMappingInfo(typeAnnotation, typeCondition, method).combine(info);
+			}
+		}
+		return info;
+	}
+
+	protected RequestMappingInfo createRequestMappingInfo(RequestMapping annotation,
+			RequestCondition<?> customCondition, Method method) {
+		String className = method.getDeclaringClass().getSimpleName();
+		String classNameExceptAction = StringUtils.uncapitalize(className.substring(0, className.length() - 10));
+		String[] patterns = resolveEmbeddedValuesInPatterns(annotation.value());
+		if ((patterns != null) && (patterns.length == 0)) {//
+			patterns = new String[] { classNameExceptAction + "!" + method.getName() };
+		}
+		System.out.println(patterns[0]);
+
+		return new RequestMappingInfo(
+				new PatternsRequestCondition(patterns, getUrlPathHelper(), getPathMatcher(), this.useSuffixPatternMatch,
+						this.useTrailingSlashMatch, this.fileExtensions),
+				new RequestMethodsRequestCondition(annotation.method()),
+				new ParamsRequestCondition(annotation.params()), new HeadersRequestCondition(annotation.headers()),
+				new ConsumesRequestCondition(annotation.consumes(), annotation.headers()), new ProducesRequestCondition(
+						annotation.produces(), annotation.headers(), this.contentNegotiationManager),
+				customCondition);
+	}
+	
+}
---
+++ src/main/java/org/springframework/biz/web/servlet/mvc/method/annotation/RoutableRequestMappingHandlerMapping.java
@@ -0,0 +1,102 @@
+package org.springframework.biz.web.servlet.mvc.method.annotation;
+
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.biz.config.Ini;
+import org.springframework.util.AntPathMatcher;
+import org.springframework.util.CollectionUtils;
+import org.springframework.util.ReflectionUtils;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
+
+@RequestMapping
+public class RoutableRequestMappingHandlerMapping extends RequestMappingHandlerMapping {
+
+	protected static Field mappingRegistryField = ReflectionUtils.findField(RequestMappingHandlerMapping.class, "mappingRegistry");
+
+	protected AntPathMatcher pathMatcher = new AntPathMatcher();
+	
+	/**
+	 * 处理器链定义
+	 */
+	private Map<String, String> handlerDefinitionMap;
+
+	static {
+		mappingRegistryField.setAccessible(true);
+	}
+
+	public RoutableRequestMappingHandlerMapping() {
+		handlerDefinitionMap = new LinkedHashMap<String, String>();
+	}
+
+	public Map<String, String> getHandlerDefinitionMap() {
+		return handlerDefinitionMap;
+	}
+
+	public void setHandlerDefinitionMap(Map<String, String> handlerDefinitionMap) {
+		this.handlerDefinitionMap = handlerDefinitionMap;
+	}
+	
+	public void setHandlerChainDefinitions(String definitions) throws IOException {
+        Ini ini = new Ini();
+        ini.load(definitions);
+        Ini.Section section = ini.getSection("urls");
+        if (CollectionUtils.isEmpty(section)) {
+            section = ini.getSection(Ini.DEFAULT_SECTION_NAME);
+        }
+        setHandlerDefinitionMap(section);
+    }
+
+	@Override
+	protected HandlerMethod getHandlerInternal(HttpServletRequest request) throws Exception {
+		// 找到当前请求路径的原始HandlerMethod
+		HandlerMethod handlerMethod = super.getHandlerInternal(request);
+		// 获取lookupPath
+		String lookupPath = getUrlPathHelper().getLookupPathForRequest(request);
+		if (logger.isDebugEnabled()) {
+			logger.debug("Looking up handler method for path " + lookupPath);
+		}
+		
+		Iterator<Entry<String, String>>  ite = handlerDefinitionMap.entrySet().iterator();
+		while (ite.hasNext()) {
+			Entry<String, String> entry = ite.next();
+			if(pathMatcher.match(entry.getKey(), lookupPath)){
+				
+				Object mappingRegistry = ReflectionUtils.getField(mappingRegistryField, this);
+				Method acquireReadLockMethod = ReflectionUtils.findMethod(mappingRegistry.getClass(), "acquireReadLock");
+				acquireReadLockMethod.setAccessible(true);
+				ReflectionUtils.invokeMethod(acquireReadLockMethod, mappingRegistry);
+
+				try {
+					handlerMethod = lookupHandlerMethod(lookupPath, request);
+					if (logger.isDebugEnabled()) {
+						if (handlerMethod != null) {
+							logger.debug("Returning handler method [" + handlerMethod + "]");
+						} else {
+							logger.debug("Did not find handler method for [" + lookupPath + "]");
+						}
+					}
+					return (handlerMethod != null ? handlerMethod.createWithResolvedBean() : null);
+				} finally {
+					Method releaseReadLockMethod = ReflectionUtils.findMethod(mappingRegistry.getClass(), "releaseReadLock");
+					releaseReadLockMethod.setAccessible(true);
+					ReflectionUtils.invokeMethod(releaseReadLockMethod, mappingRegistry);
+				}
+				
+			}
+		}
+		
+		return handlerMethod;
+		
+	}
+
+}
