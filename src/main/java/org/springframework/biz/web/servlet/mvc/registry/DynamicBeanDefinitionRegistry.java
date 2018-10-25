5f06daa6558e330d6af6d7e0f875ddb91802850e
vindell <hnxyhcwdl1003@163.com>
2018-10-25 11:29:27 +0800

    1.0.2.RELEASE

---
+++ src/main/java/org/springframework/biz/web/servlet/mvc/registry/DynamicBeanDefinitionRegistry.java
@@ -0,0 +1,72 @@
+package org.springframework.biz.web.servlet.mvc.registry;
+
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+
+public interface DynamicBeanDefinitionRegistry extends BeanDefinitionRegistry {
+
+	/**
+     * 动态注册指定类型的Class到Spring上下文
+     * @param beanClass			: The class type of bean 
+     */
+    public void registerBean(Class<?> beanClass);
+    
+    /**
+     * 动态注册指定类型的Class到Spring上下文
+     * @param beanClass			: The class type of bean
+     * @param scope				: scope value
+     */
+    public void registerBean(Class<?> beanClass, String scope); 
+    
+    /**
+     * 动态注册指定类型的Class到Spring上下文
+     * @param beanClass			: The class type of bean
+     * @param scope				: scope value
+     * @param lazyInit			: lazyInit value
+     */
+    public void registerBean(Class<?> beanClass, String scope, boolean lazyInit); 
+    
+    /**
+     * 动态注册指定类型的Class到Spring上下文
+     * @param beanClass			: The class type of bean
+     * @param scope				: scope value
+     * @param lazyInit			: lazyInit value
+     * @param autowireCandidate	: autowireCandidate value
+     */
+    public void registerBean(Class<?> beanClass, String scope, boolean lazyInit,boolean autowireCandidate); 
+    
+    /**
+     * 动态注册指定类型的Class到Spring上下文
+     * @param beanName			: The name of bean 
+     * @param beanClass			: The class type of bean
+     */
+    public void registerBean(String beanName, Class<?> beanClass);
+	
+    /**
+     * 动态注册指定类型的Class到Spring上下文
+     * @param beanName			: The name of bean 
+     * @param beanClass			: The class type of bean
+     * @param scope				: scope value
+     */
+    public void registerBean(String beanName, Class<?> beanClass, String scope); 
+    
+    /**
+     * 动态注册指定类型的Class到Spring上下文
+     * @param beanName			: The name of bean 
+     * @param beanClass			: The class type of bean
+     * @param scope				: scope value
+     * @param lazyInit			: lazyInit value
+     */
+    public void registerBean(String beanName, Class<?> beanClass, String scope, boolean lazyInit); 
+    
+    /**
+     * 动态注册指定类型的Class到Spring上下文
+     * @param beanName			: The name of bean 
+     * @param beanClass			: The class type of bean
+     * @param scope				: scope value
+     * @param lazyInit			: lazyInit value
+     * @param autowireCandidate	: autowireCandidate value
+     */
+    public void registerBean(String beanName, Class<?> beanClass, String scope, boolean lazyInit,boolean autowireCandidate); 
+    
+    
+}
