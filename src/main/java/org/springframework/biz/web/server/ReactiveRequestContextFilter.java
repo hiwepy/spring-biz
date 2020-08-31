3e8749c6edbc5534926165891b0034d524a718b7
wandl <hnxyhcwdl1003@163.com>
2020-09-01 00:05:11 +0800

    增加逻辑

---
+++ src/main/java/org/springframework/biz/web/server/ReactiveRequestContextFilter.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2018, hiwepy (https://github.com/hiwepy).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.springframework.biz.web.server;
+
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+
+import reactor.core.publisher.Mono;
+
+/**
+ * https://developer.aliyun.com/article/696829
+ * ReactiveRequestContextFilter
+ * @author L.cm
+ */
+public class ReactiveRequestContextFilter implements WebFilter {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+        ServerHttpRequest request = exchange.getRequest();
+        ServerHttpResponse response = exchange.getResponse();
+        return chain.filter(exchange)
+            .subscriberContext(ctx -> ctx.put(ReactiveRequestContextHolder.EXCHANGE_KEY, exchange))
+            .subscriberContext(ctx -> ctx.put(ReactiveRequestContextHolder.REQUEST_KEY, request))
+            .subscriberContext(ctx -> ctx.put(ReactiveRequestContextHolder.RESPONSE_KEY, response));
+    }
+    
+}
---
+++ src/main/java/org/springframework/biz/web/server/i18n/NestedLocaleContextResolver.java
@@ -0,0 +1,58 @@
+package org.springframework.biz.web.server.i18n;
+
+import java.util.List;
+
+import org.springframework.context.i18n.LocaleContext;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.i18n.LocaleContextResolver;
+
+/**
+ * 嵌套Locale解析器；解决同时设置Locale到Session和Cookie的问题
+ */
+public class NestedLocaleContextResolver implements LocaleContextResolver {
+	
+	protected List<LocaleContextResolver> resolvers;
+
+	protected boolean isNested() {
+		if(getResolvers() != null){
+			return true;
+		}
+		return false;
+	}
+	
+	public List<LocaleContextResolver> getResolvers() {
+		return resolvers;
+	}
+
+	public void setResolvers(List<LocaleContextResolver> resolvers) {
+		this.resolvers = resolvers;
+	}
+
+	@Override
+	public LocaleContext resolveLocaleContext(ServerWebExchange exchange) {
+		if(isNested()){
+			for (LocaleContextResolver localeResolver : getResolvers()) {
+				//解析locale
+				LocaleContext localeContext = localeResolver.resolveLocaleContext(exchange);
+				if(localeContext == null){
+					continue;
+				}
+				LocaleContextHolder.setLocaleContext(localeContext);
+				return localeContext;
+			}
+		}
+		return null;
+	}
+
+	@Override
+	public void setLocaleContext(ServerWebExchange exchange, LocaleContext localeContext) {
+		if(isNested()){
+			for (LocaleContextResolver localeResolver : getResolvers()) {
+				localeResolver.setLocaleContext(exchange, localeContext);
+			}
+		}
+		
+	}
+
+}
