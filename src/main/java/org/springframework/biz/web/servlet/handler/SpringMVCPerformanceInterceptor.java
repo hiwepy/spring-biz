8eb461866cf426ab9f517ce6a31b7f22bf3bccdc
wandl <hiwepy@163.com>
2021-06-29 13:17:38 +0800

    引用优化

--- src/main/java/org/springframework/biz/web/servlet/handler/SpringMVCPerformanceInterceptor.java
+++ src/main/java/org/springframework/biz/web/servlet/handler/SpringMVCPerformanceInterceptor.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.InitializingBean;
-import org.springframework.biz.utils.RemoteAddrUtils;
+import org.springframework.biz.utils.WebUtils;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.ApplicationEventPublisherAware;
 import org.springframework.core.NamedThreadLocal;
@@ -82,7 +82,7 @@ public class SpringMVCPerformanceInterceptor extends HandlerInterceptorAdapter i
 		 */
 		
 		RequestHandledEvent event = new ServletRequestHandledEvent(this, request.getRequestURL().toString(),
-				RemoteAddrUtils.getRemoteAddr(request), request.getMethod(), request.getServerName(),
+				WebUtils.getRemoteAddr(request), request.getMethod(), request.getServerName(),
 				request.getRequestedSessionId(), request.getRemoteUser(), processingTimeMillis);
 		getEventPublisher().publishEvent(event);
 		
